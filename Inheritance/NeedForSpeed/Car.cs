c9b99e6b6943bc4f7d2508ea3894010e23911a1c
Milena Alexieva <doycheva_m@abv.bg>
2020-12-17 18:51:48 +0200

    Add files via upload

---
+++ Inheritance/NeedForSpeed/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Car:Vehicle
+    {
+       
+        public Car(int horsePower, double fuel):base(horsePower,fuel)
+        {
+            this.DefaultFuelConsumption = 3;
+        }
+
+        public override double FuelConsumption
+        {
+            get => base.FuelConsumption; 
+            set => base.FuelConsumption = value;
+        }
+
+        public override void Drive(double kilometers)
+        {
+            this.Fuel -= this.FuelConsumption * kilometers;
+        }
+    }
+}
