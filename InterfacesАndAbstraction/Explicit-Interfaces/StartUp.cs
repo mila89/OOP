6c9a46768c095af292db4d004178e3418c658390
Milena Alexieva <doycheva_m@abv.bg>
2020-12-31 18:06:20 +0200

    Add files via upload

---
+++ InterfacesАndAbstraction/Explicit-Interfaces/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Explicit_Interfaces
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+            var list = new List<Citizen>();
+            while (input[0]!="End")
+            {
+                string name = input[0];
+                string country = input[1];
+                int age = int.Parse(input[2]);
+                list.Add(new Citizen(name, country, age)); 
+                input = Console.ReadLine().Split();
+            }
+
+            foreach (var citizen in list)
+            {
+                var controlPerson = citizen as IPerson;
+                Console.WriteLine(controlPerson.GetName());
+                var controlResident = citizen as IResident;
+                Console.WriteLine(controlResident.GetName());
+
+            }
+        }
+    }
+}
