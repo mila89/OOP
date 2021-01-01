96979b1b0c4df32922bbc66615f7f17b9519d805
Milena Alexieva <doycheva_m@abv.bg>
2021-01-01 22:51:46 +0200

    Add files via upload

---
+++ Polymorphism/Vehicles/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Car : Vehicles
+    {
+        private double quantity;
+        private readonly double consumption;
+
+        public double Quantity { get => this.quantity; }
+           
+        public Car(double quantity, double consumption)
+        {
+            this.consumption=consumption+0.9;
+            this.quantity = quantity;
+        }
+        public override void Driving(double distance)
+        {
+            if (this.consumption * distance <= this.quantity)
+            {
+                this.quantity -= this.consumption * distance;
+                Console.WriteLine($"{GetType().Name} travelled {distance} km");
+            }
+            else
+                Console.WriteLine($"{GetType().Name} needs refueling");
+        }
+
+        public override void Refueling(double litters)
+        {
+            this.quantity += litters;
+        }
+    }
+}
---
+++ Polymorphism/Vehicles/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Vehicles
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] inputCar = Console.ReadLine().Split();
+            string[] inputTruck = Console.ReadLine().Split();
+            Car currentCar = new Car(double.Parse(inputCar[1]), double.Parse(inputCar[2]));
+            Truck currentTruck = new Truck(double.Parse(inputTruck[1]), double.Parse(inputTruck[2]));
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+                if (command[1] == "Car")
+                {
+                    if (command[0] == "Drive")
+                    {
+                        currentCar.Driving(double.Parse(command[2]));
+                    }
+                    else if (command[0] == "Refuel")
+                    {
+                        currentCar.Refueling(double.Parse(command[2]));
+                    }
+                }
+                else if (command[1] == "Truck")
+                {
+                    if (command[0] == "Drive")
+                    {
+                        currentTruck.Driving(double.Parse(command[2]));
+                    }
+                    else if (command[0] == "Refuel")
+                    {
+                        currentTruck.Refueling(double.Parse(command[2]));
+                    }
+                }
+            }
+            Console.WriteLine($"Car: {Math.Round(currentCar.Quantity,2,MidpointRounding.AwayFromZero):f2}");
+            Console.WriteLine($"Truck: {Math.Round(currentTruck.Quantity,2,MidpointRounding.AwayFromZero):f2}");
+        }
+    }
+}
