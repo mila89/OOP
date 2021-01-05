4f5da6ffe98be0e0fd63af82db7f648d03822b52
Milena Alexieva <doycheva_m@abv.bg>
2021-01-05 23:43:09 +0200

    Add files via upload

--- Reflection and Atributes/Stealer/Spy.cs
+++ Reflection and Atributes/Stealer/Spy.cs
@@ -18,11 +18,34 @@ namespace Stealer
             Object spyInstance = Activator.CreateInstance(spyType, new object[] { });
             foreach (FieldInfo field in spyFields.Where(f=>fieldsName.Contains(f.Name)))
             {
-                sb.AppendLine($"{field.Name} ={field.GetValue(spyInstance)}");
+                sb.AppendLine($"{field.Name} = {field.GetValue(spyInstance)}");
             }
             
             return sb.ToString();
 
+        }
+
+        public string AnalyzeAcessModifiers(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+            Type spyType = Type.GetType(className);
+            Object instance=Activator.CreateInstance(spyType, new object[]{ });
+            FieldInfo[] spyFields = spyType.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            foreach (FieldInfo field in spyFields)
+            {
+                sb.AppendLine($"{field.Name} must be private!");
+            }
+            MethodInfo[] getMethods = spyType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            foreach (MethodInfo method in getMethods.Where(m=> m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} have to be public!");
+            }
+            MethodInfo[] setMethods = spyType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            foreach (MethodInfo method in setMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} have to be private!");
+            }
+            return sb.ToString();
         }
     }
 }
