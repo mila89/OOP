ba71e8965a8589b27113cc59fbb9a67ba47d4145
Milena Alexieva <doycheva_m@abv.bg>
2021-01-08 16:22:05 +0200

    Add files via upload

---
+++ SOLID/P02.Graphic_Editor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P02.Graphic_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+            IDrawable rectangle = new Rectangle();
+            IDrawable circle = new Circle();
+            GraphicEditor graphicEditor =new GraphicEditor();
+            graphicEditor.DrawShape(circle);
+            graphicEditor.DrawShape(rectangle);
+        }
+    }
+}
