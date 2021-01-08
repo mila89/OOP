36d2d00d5c716ef4e0bdceb2232859a86ed9f2e1
Milena Alexieva <doycheva_m@abv.bg>
2021-01-08 16:43:53 +0200

    Add files via upload

---
+++ SOLID/P03.Detail_Printer/IPrintable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03.Detail_Printer
+{
+    public interface IPrintable
+    {
+    }
+}
---
+++ SOLID/P03.Detail_Printer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using P03.Detail_Printer;
+
+namespace P03.DetailPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            IList<IPrintable> employees = new List<IPrintable>();
+            employees.Add(new Manager("Milena", new List<string>{ }));
+            employees.Add(new Employee("Ivan Ivanov"));
+            DetailsPrinter printer = new DetailsPrinter(employees);
+            printer.PrintDetails();
+        }
+    }
+}
