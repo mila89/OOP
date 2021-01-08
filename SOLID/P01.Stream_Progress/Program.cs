1935ddca11a95d42a60f3334984573ed21c52a4d
Milena Alexieva <doycheva_m@abv.bg>
2021-01-08 13:58:41 +0200

    Add files via upload

---
+++ SOLID/P01.Stream_Progress/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Music music = new Music("Milena", "MyFirstSOLID", 20, 3000);
+            File file = new File("SOLID", 20, 300);
+            StreamProgressInfo musicStream= new StreamProgressInfo(music);
+            StreamProgressInfo fileStream = new StreamProgressInfo(file);
+            Console.WriteLine(musicStream.CalculateCurrentPercent());
+            Console.WriteLine(fileStream.CalculateCurrentPercent());
+        }
+    }
+}
