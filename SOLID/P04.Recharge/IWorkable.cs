72dfe34c178fc9358d6603126878d8c7798022bb
Milena Alexieva <doycheva_m@abv.bg>
2021-01-09 16:31:42 +0200

    Add files via upload

---
+++ SOLID/P04.Recharge/IWorkable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P04.Recharge
+{
+    public interface IWorkable
+    {
+        void Work(int hours);
+        
+    }
+}
---
+++ SOLID/P04.Recharge/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P04.Recharge
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee("Mila Akexieva");
+            Robot robot = new Robot("R500", 80);
+            RechargeStation rechargeStation = new RechargeStation();
+            rechargeStation.Recharge(robot);
+
+            employee.Work(8);
+            robot.Work(24);
+            Console.WriteLine(robot.CurrentPower);
+        }
+    }
+}
