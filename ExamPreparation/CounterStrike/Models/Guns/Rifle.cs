66a86f9d17d45c70fefa6a58dc31c16bcf0c608b
Milena Alexieva <doycheva_m@abv.bg>
2021-03-23 18:41:33 +0200

    Add files via upload

---
+++ ExamPreparation/CounterStrike/Models/Guns/Rifle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CounterStrike.Models.Guns
+{
+    public class Rifle : Gun
+    {
+        private int bulletsAtFire = 10;
+        public Rifle(string name, int bullets) : base(name, bullets)
+        {
+        }
+        public override int Fire()
+        {
+            if (this.BulletsCount - bulletsAtFire >= 0)
+            {
+                this.BulletsCount-=bulletsAtFire;
+            }
+            else
+            {
+                bulletsAtFire = BulletsCount;
+                this.BulletsCount = 0;
+            }
+            return bulletsAtFire;
+        }
+    }
+}
