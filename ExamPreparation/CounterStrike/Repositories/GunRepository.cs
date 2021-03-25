e5f35dbb4221d325eda26022a7dd0ecf8f36e768
Milena Alexieva <doycheva_m@abv.bg>
2021-03-25 18:10:03 +0200

    Add files via upload

---
+++ ExamPreparation/CounterStrike/Repositories/GunRepository.cs
@@ -0,0 +1,43 @@
+﻿using CounterStrike.Models.Guns.Contracts;
+using CounterStrike.Repositories.Contracts;
+using CounterStrike.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CounterStrike.Repositories
+{
+    public class GunRepository : IRepository<IGun>
+    {
+        private List<IGun> guns;
+
+        public GunRepository()
+        {
+            guns = new List<IGun>();
+        }
+        public IReadOnlyCollection<IGun> Models 
+        {
+            get { return this.guns.AsReadOnly();
+            }
+        }
+
+        public void Add(IGun gun)
+        {
+            if (gun is null)
+                throw new ArgumentException(ExceptionMessages.InvalidGunRepository);
+            guns.Add(gun);
+            
+        }
+
+        public IGun FindByName(string name)
+        {
+            return this.guns.FirstOrDefault(g => g.Name == name);
+        }
+
+        public bool Remove(IGun model)
+        {
+            return this.guns.Remove(model);
+        }
+    }
+}
