b60f714a80a1bd1f2eb3cc86bc0eba4c84b1e5c2
Milena Alexieva <doycheva_m@abv.bg>
2021-04-11 20:17:30 +0300

    Add files via upload

---
+++ ExamPreparation/Exam_August_2020/Easter Races/Models/Cars/Entities/SportsCar.cs
@@ -0,0 +1,35 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Cars.Entities
+{
+    
+    public class SportsCar:Car,ICar
+    {
+        //private int horsePower;
+        private int minHorsePower;
+        private int maxHorsePower;
+        public SportsCar(string model, int horsePower)
+            : base(model)
+        {
+            this.minHorsePower = 250;
+            this.maxHorsePower = 450;
+            this.HorsePower = horsePower;
+            this.cubicCentimeters = 3000;
+        }
+
+        public override int HorsePower 
+        {
+            get { return this.horsePower; }
+            protected set
+            {
+                if (value > this.maxHorsePower || value < this.minHorsePower)
+                {
+                    throw new ArgumentException($"Invalid horse power: {value}.");
+                }
+            }
+        }
+    }
+}
