fd0b90300bbdc1624d674222e10579c05f3c2d87
Milena Alexieva <doycheva_m@abv.bg>
2021-04-15 11:05:14 +0300

    Add files via upload

---
+++ ExamPreparation/Exam_August_2020/Easter Races/Repositories/Entities/Repository.cs
@@ -0,0 +1,43 @@
+﻿using EasterRaces.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public abstract class Repository<T>:IRepository<T>
+    {
+        private List<T> models;
+
+        public Repository()
+        {
+            models = new List<T>();     
+        }
+        public void Add(T model)
+        {
+            if (model == null) // to check
+                throw new ArgumentNullException(GetNullNameValidationMessage());
+            this.models.Add(model);
+        }
+
+        public IReadOnlyCollection<T> GetAll()
+        {
+            return this.models.AsReadOnly();
+        }
+
+        public T GetByName(string name)
+        {
+            return this.models.FirstOrDefault(FindByNameDelegate(name));
+        }
+
+        public bool Remove(T model)
+        {
+
+           return this.models.Remove(model);
+        }
+
+        protected abstract string GetNullNameValidationMessage();
+        protected abstract Func<T, bool> FindByNameDelegate(string name);
+    }
+}
