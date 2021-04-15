cb1b950e17f76b3bca66b2e9a75837649de231b9
Milena Alexieva <doycheva_m@abv.bg>
2021-04-15 11:03:33 +0300

    Add files via upload

---
+++ ExamPreparation/Exam_August_2020/Easter Races/Repositories/Entities/DriverRepository.cs
@@ -0,0 +1,20 @@
+﻿using EasterRaces.Models.Drivers.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class DriverRepository : Repository<IDriver>
+    {
+        protected override Func<IDriver, bool> FindByNameDelegate(string name)
+        {
+            return x => x.Name == name;
+        }
+
+        protected override string GetNullNameValidationMessage()
+        {
+            return "Driver cannot be null.";
+        }
+    }
+}
