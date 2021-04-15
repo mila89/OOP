3ffd6725854e2a0bcae29b9ae6a57c6fb136c9b4
Milena Alexieva <doycheva_m@abv.bg>
2021-04-15 11:04:48 +0300

    Add files via upload

---
+++ ExamPreparation/Exam_August_2020/Easter Races/Repositories/Entities/RaceRepository.cs
@@ -0,0 +1,20 @@
+﻿using EasterRaces.Models.Races.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class RaceRepository : Repository<IRace>
+    {
+        protected override Func<IRace, bool> FindByNameDelegate(string name)
+        {
+            return x => x.Name == name;
+        }
+
+        protected override string GetNullNameValidationMessage()
+        {
+            return "Race cannot be null.";
+        }
+    }
+}
