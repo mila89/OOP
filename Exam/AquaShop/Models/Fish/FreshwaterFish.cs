0dbbb7051b82b227c8de0e21753dd87a0d52140c
Milena Alexieva <doycheva_m@abv.bg>
2021-05-05 21:20:57 +0300

    Add files via upload

---
+++ Exam/AquaShop/Models/Fish/FreshwaterFish.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Fish
+{
+    public class FreshwaterFish:Fish
+    {
+        public FreshwaterFish(string name, string species, decimal price)
+                        :base(name, species, price)
+        {
+            this._size = 3;
+            //Can only live in FreshwaterAquarium!
+        }
+
+        public override void Eat()
+        {
+            this._size += 3;
+        }
+
+        
+    }
+}
