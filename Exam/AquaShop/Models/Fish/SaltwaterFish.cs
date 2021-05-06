e873b985f502433764e6512193e892f5a054d2c4
Milena Alexieva <doycheva_m@abv.bg>
2021-05-06 21:06:36 +0300

    Add files via upload

---
+++ Exam/AquaShop/Models/Fish/SaltwaterFish.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Fish
+{
+    public class SaltwaterFish:Fish
+    {
+        public SaltwaterFish(string name, string species, decimal price)
+                            :base(name, species, price)
+        {
+            this._size = 5;
+            //Can only live in SaltwaterAquarium!
+        }
+
+        public override void Eat()
+        {
+            this._size += 2;
+        }
+    }
+}
